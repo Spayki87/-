d5c94dda356035cd3e8e9645716dd0c3fb64df9f
Spayki87 <136745298+Spayki87@users.noreply.github.com>
2023-06-18 10:59:50 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name;
+            int age;
+            string zodiacSigh;
+            string job;
+            Console.Write("Введите ваше имя:");
+            name = Console.ReadLine();
+            Console.Write("Введите ваш возраст:");
+            age = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Введите ваш знак зодиака:");
+            zodiacSigh = Console.ReadLine();
+            Console.Write("Введите место вашей работы:");
+            job = Console.ReadLine();
+            Console.WriteLine("Вас зовут - " + name + ", вам - " + age + " лет, по зодиаку вы " + zodiacSigh + " и работаете в " + job + ".");
+        }
+    }
+}
